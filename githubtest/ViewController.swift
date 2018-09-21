4c46669b37fea7a79173d4562d8aea47e70525c9
amirdivanbeigi <32493593+amirdivanbeigi@users.noreply.github.com>
2018-09-21 11:02:56 +0430

    Initial Commit

---
+++ githubtest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  githubtest
+//
+//  Created by amir on 9/21/18.
+//  Copyright © 2018 amirplayingcard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
